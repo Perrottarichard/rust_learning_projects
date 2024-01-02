36a10e0cc6bd291b0701e4703123b899ed306e3d
Perrottarichard <57297531+Perrottarichard@users.noreply.github.com>
2024-01-01 21:05:40 -0600

    impl

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,12 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn w_pow(&self, exp: u32) -> u32 {
+        self.width.pow(exp)
+    }
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        return &self.width > &rect.width && &self.height > &rect.height;
+    }
 }
 
 fn main() {
@@ -15,9 +21,15 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
